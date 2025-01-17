7e5b20dd26004a44344f0af25d80ecac44f96b1b
Amol Bhave <amol@amolbhave.com>
2025-01-17 09:23:06 -0500

    add markdown support in the comment box

--- src/components/comment-box.tsx
+++ src/components/comment-box.tsx
@@ -2,6 +2,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { DotsVerticalIcon } from "@radix-ui/react-icons";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import { toast } from "sonner";
 import { z } from "zod";
 
@@ -15,6 +17,7 @@ import {
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
 import { trpc } from "@/lib/trpc";
+import { cn } from "@/lib/utils";
 
 const DEFAULT_MESSAGE = "No comment set.";
 
@@ -85,7 +88,189 @@ export function CommentBox({
       <span>
         {!isEditingComment ? (
           <span>
-            {updatedComment}
+            <div className="text-sm">
+              <Markdown
+                remarkPlugins={[remarkGfm]}
+                components={{
+                  h1: ({ children, className, node: _node, ...rest }) => (
+                    <h1
+                      {...rest}
+                      className={cn(
+                        className,
+                        "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+                      )}
+                    >
+                      {children}
+                    </h1>
+                  ),
+                  h2: ({ children, className, node: _node, ...rest }) => (
+                    <h2
+                      {...rest}
+                      className={cn(
+                        className,
+                        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+                      )}
+                    >
+                      {children}
+                    </h2>
+                  ),
+                  h3: ({ children, className, node: _node, ...rest }) => (
+                    <h3
+                      {...rest}
+                      className={cn(
+                        className,
+                        "scroll-m-20 text-2xl font-semibold tracking-tight",
+                      )}
+                    >
+                      {children}
+                    </h3>
+                  ),
+                  h4: ({ children, className, node: _node, ...rest }) => (
+                    <h4
+                      {...rest}
+                      className={cn(
+                        className,
+                        "scroll-m-20 text-xl font-semibold tracking-tight",
+                      )}
+                    >
+                      {children}
+                    </h4>
+                  ),
+                  h5: ({ children, className, node: _node, ...rest }) => (
+                    <h4
+                      {...rest}
+                      className={cn(
+                        className,
+                        "scroll-m-20 text-lg font-semibold tracking-tight",
+                      )}
+                    >
+                      {children}
+                    </h4>
+                  ),
+                  h6: ({ children, className, node: _node, ...rest }) => (
+                    <h4
+                      {...rest}
+                      className={cn(
+                        className,
+                        "scroll-m-20 text-base font-semibold tracking-tight",
+                      )}
+                    >
+                      {children}
+                    </h4>
+                  ),
+                  p: ({ children, className, node: _node, ...rest }) => (
+                    <p
+                      {...rest}
+                      className={cn(
+                        className,
+                        "leading-7 [&:not(:first-child)]:mt-6",
+                      )}
+                    >
+                      {children}
+                    </p>
+                  ),
+                  blockquote: ({
+                    children,
+                    className,
+                    node: _node,
+                    ...rest
+                  }) => (
+                    <blockquote
+                      {...rest}
+                      className={cn(className, "mt-6 border-l-2 pl-6 italic")}
+                    >
+                      {children}
+                    </blockquote>
+                  ),
+                  ul: ({ children, className, node: _node, ...rest }) => (
+                    <ul
+                      {...rest}
+                      className={cn(
+                        className,
+                        "my-6 ml-6 list-disc [&>li]:mt-2",
+                      )}
+                    >
+                      {children}
+                    </ul>
+                  ),
+                  ol: ({ children, className, node: _node, ...rest }) => (
+                    <ol
+                      {...rest}
+                      className={cn(
+                        className,
+                        "my-6 ml-6 list-decimal [&>li]:mt-2",
+                      )}
+                    >
+                      {children}
+                    </ol>
+                  ),
+                  code: ({ children, className, node: _node, ...rest }) => (
+                    <code
+                      {...rest}
+                      className={cn(
+                        className,
+                        "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+                      )}
+                    >
+                      {children}
+                    </code>
+                  ),
+                  table: ({ children, className, node: _node, ...rest }) => (
+                    <div className="my-6 w-full overflow-y-auto">
+                      <table {...rest} className={cn(className, "w-full")}>
+                        {children}
+                      </table>
+                    </div>
+                  ),
+                  tr: ({ children, className, node: _node, ...rest }) => (
+                    <tr
+                      {...rest}
+                      className={cn(
+                        className,
+                        "m-0 border-t p-0 even:bg-muted",
+                      )}
+                    >
+                      {children}
+                    </tr>
+                  ),
+                  td: ({ children, className, node: _node, ...rest }) => (
+                    <td
+                      {...rest}
+                      className={cn(
+                        className,
+                        "border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
+                      )}
+                    >
+                      {children}
+                    </td>
+                  ),
+                  th: ({ children, className, node: _node, ...rest }) => (
+                    <th
+                      {...rest}
+                      className={cn(
+                        className,
+                        "border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
+                      )}
+                    >
+                      {children}
+                    </th>
+                  ),
+                  a: ({ children, className, node: _node, ...rest }) => (
+                    <a
+                      {...rest}
+                      className={cn(
+                        className,
+                        "font-medium underline underline-offset-4",
+                      )}
+                    >
+                      {children}
+                    </a>
+                  ),
+                }}
+              >
+                {updatedComment}
+              </Markdown>
+            </div>
             <span className="float-right">
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
