cd0cc49a6c42a8c4c04b60905c254bcd0ceb37dd
Allen Park <allenpark93@gmail.com>
2026-01-25 14:48:34 -0600

    Solves #117 and #129

--- src/components/help-page.tsx
+++ src/components/help-page.tsx
@@ -4,20 +4,29 @@ export function HelpPage() {
       <div className="max-w-4xl">
         <h2>Help Page</h2>
         <p />
-        <h4>Swipe between pages</h4>
+        <h4 id="swipe">Swipe between pages</h4>
         <p>
           To turn off swipe between pages on Chrome, visit this link to toggle the option:
           chrome://settings/?search=swipe+between+pages
         </p>
         <p />
-        <h4>Shortcut Command Bar</h4>
+        <h4 id="shortcut">Shortcut Command Bar</h4>
         <p>
           Press <code>Cmd + K</code> (Mac) or <code>Ctrl + K</code> (Windows) to open the command
           bar. From there, you can quickly navigate to different sections of the app or perform
           actions.
         </p>
         <p />
-        <h4>File issues</h4>
+        <h4 id="wifi">MIT Wi-Fi Discord Voice Channel Issues</h4>
+        <p>
+          For anyone have issues connecting to Discord voice channels on MIT GUEST: If you are an
+          MIT alum or associate, go to <a href="https://wifi.mit.edu/">https://wifi.mit.edu/</a> and
+          use your alum login to get the password to "MIT" network. If you are not affiliated with
+          MIT, then sign into eduroam with your home institution credentials (ask around to borrow
+          one if you don't have one).
+        </p>
+        <p />
+        <h4 id="file-issues">File issues</h4>
         <p>
           File issues by talking to Amol or Allen or by filing a{" "}
           <a
