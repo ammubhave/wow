16d478d3d02f920d461b44d7b32be21c23267de1
Amol Bhave <amol@amolbhave.com>
2026-01-27 21:54:44 -0600

    add google login ability

--- src/routes/_public/login.tsx
+++ src/routes/_public/login.tsx
@@ -6,8 +6,9 @@ import z from "zod";
 
 import {useAppForm} from "@/components/form";
 import {useTheme} from "@/components/theme-provider";
+import {Button} from "@/components/ui/button";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {Field, FieldDescription, FieldGroup} from "@/components/ui/field";
+import {Field, FieldDescription, FieldGroup, FieldSeparator} from "@/components/ui/field";
 import {authClient} from "@/lib/auth-client";
 import {getSession} from "@/lib/auth-server";
 
@@ -20,6 +21,10 @@ export const Route = createFileRoute("/_public/login")({
     if (session) throw redirect({to: "/workspaces"});
   },
 });
+
+async function loginWithGoogle() {
+  await authClient.signIn.social({provider: "google", callbackURL: "/workspaces"}, {throw: true});
+}
 
 function RouteComponent() {
   const router = useRouter();
@@ -52,12 +57,28 @@ function RouteComponent() {
           <Card>
             <CardHeader>
               <CardTitle>Login to your account</CardTitle>
-              <CardDescription>Enter your email below to login to your account</CardDescription>
+              <CardDescription>
+                You can login with Google or use your email and password below.
+              </CardDescription>
             </CardHeader>
             <CardContent>
               <form.AppForm>
                 <form.Form>
                   <FieldGroup>
+                    <Field>
+                      <Button variant="outline" onClick={loginWithGoogle}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                          <path
+                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                            fill="currentColor"
+                          />
+                        </svg>
+                        Login with Google
+                      </Button>
+                    </Field>
+                    <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                      Or continue with
+                    </FieldSeparator>
                     <form.AppField name="email">
                       {field => <field.TextField label="Email" autoComplete="email" />}
                     </form.AppField>
